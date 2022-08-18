35a8dc74e4e7b87634bd65cd15c326fe668d847d
Youka <72034970+yrnmsk@users.noreply.github.com>
2022-08-19 03:07:21 +0800

    chore: clean code

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
-const dimensions = 16;
 const square = document.querySelector('.square');
 const pixel = document.createElement('div');
 pixel.classList.add('pixel');
 
-for (let i = 0; i < dimensions; ++i) {
-  const row = document.createElement('div');
-  row.classList.add('row');
-  for (let j = 0; j < dimensions; ++j) {
-    const child = document.createElement('div');
-    child.classList.add('pixel');
-    row.appendChild(child);
-  }
-  square.appendChild(row);
-}
-
+const range = document.querySelector('#range');
+const label = document.querySelector('#label');
+let dimensions = range.value;
 let isMouseDown = false;
-const pixels = Array.from(document.querySelectorAll('.pixel'));
+let pixels;
 
 square.addEventListener('mousedown', () => isMouseDown = true);
 square.addEventListener('mouseup', () => isMouseDown = false);
 square.addEventListener('mouseleave', () => isMouseDown = false);
 
-pixels.forEach(pxl => pxl.addEventListener('mouseover', event => {
-  if (!isMouseDown) return;
-  console.log(event.target);
-}))
+const renderPixels = dimensions => {
+  for (let i = 0; i < dimensions; ++i) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    for (let j = 0; j < dimensions; ++j) {
+      const child = document.createElement('div');
+      child.classList.add('pixel');
+      row.appendChild(child);
+    }
+    square.appendChild(row);
+  }
+};
+
+const drawPixels = () => {
+  pixels = Array.from(document.querySelectorAll('.pixel'));
+  pixels.forEach(pxl => pxl.addEventListener('mouseover', event => {
+    if (!isMouseDown) return;
+    console.log(event.target);
+  }))
+};
+
+range.addEventListener('input', event => {
+  label.textContent = event.target.value;
+  dimensions = event.target.value;
+});
+
+range.addEventListener('change', () => {
+  Array.from(square.children).forEach(child => square.removeChild(child));
+  renderPixels(dimensions);
+  drawPixels();
+});
+
+renderPixels(dimensions);
+drawPixels();
